55784876a9d63242d6e52eeed80444726d0faba4
taibislamdipu <mailtaibislam@gmail.com>
2020-10-13 05:58:39 +0600

    initial repo setup

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Creative Network is coming soon!</h1>
+        </div>
+    );
+};
+
+export default Home;
